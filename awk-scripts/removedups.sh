5dcae5b8fe368ea9d57aa41edf40fbe11563f3eb
moufki <moufki@8a65f31f-a56f-4bd6-98e8-2755f64920ec>
2010-04-11 09:07:07 +0000

    Update on removedups.sh script: -added .$$ to temp files in order to be uniques -changed pkgs (packages) to pckts (packets)

--- awk-scripts/removedups.sh
+++ awk-scripts/removedups.sh
@@ -24,32 +24,31 @@ FRAMEDIR=$2
 TMPDIR=$3
 
 #echo Making md5sums >&2
-LASTDIR=$(pwd)
-cd $FRAMEDIR
-for i in Frame*html; do
-  #echo $i >&2
-  MD5SUM=$(mkmd5sum $i)
-  N=${i##Frame}		# ## is used to remove Frame from FrameXX.html (N is like XX.html)
-  #echo $N >&2
-  sed "s/-$//;s/^/${N%%.html} /" <<< $MD5SUM
-done > $TMPDIR/md5sums
-cd $LASTDIR
+( cd $FRAMEDIR
+  for i in Frame*html; do
+    #echo $i >&2
+    MD5SUM=$(mkmd5sum $i)
+    N=${i##Frame}		# ## is used to remove Frame from FrameXX.html (N is like XX.html)
+    #echo $N >&2
+    sed "s/-$//;s/^/${N%%.html} /" <<< $MD5SUM
+  done > $TMPDIR/md5sums.$$
+)
 
 #echo Looking for unique frames >&2
-for M in $(awk '{print $2}' $TMPDIR/md5sums | sort -u); do
-  grep $M $TMPDIR/md5sums | head -1
-done |awk '{print $1}' | sort -n  > $TMPDIR/pkgs
+for M in $(awk '{print $2}' $TMPDIR/md5sums.$$ | sort -u); do
+  grep $M $TMPDIR/md5sums.$$ | head -1
+done | awk '{print $1}' | sort -n  > $TMPDIR/pckts.$$
 
 #echo Trick on callflow.short
-sed 's/^ *//' $DESTDIR/callflow.short > $TMPDIR/callflow.short.dup
+sed 's/^ *//' $DESTDIR/callflow.short > $TMPDIR/callflow.short.$$
 
 # Join the files
-for i in $(cat $TMPDIR/pkgs); do
-  grep "^$i " $TMPDIR/callflow.short.dup
+for i in $(cat $TMPDIR/pckts.$$); do
+  grep "^$i " $TMPDIR/callflow.short.$$
 done > $DESTDIR/callflow.short.new
 
 #Remove temp files
-rm -f $TMPDIR/md5sums
-rm -f $TMPDIR/pkgs
-rm -f $TMPDIR/callflow.short.dup
+rm -f $TMPDIR/md5sums.$$
+rm -f $TMPDIR/pckts.$$
+rm -f $TMPDIR/callflow.short.$$
 
