6cab3cd77336cf58f629747fd2f92aab821b119f
rbos <rbos@8a65f31f-a56f-4bd6-98e8-2755f64920ec>
2010-04-27 19:20:17 +0000

    Report the number of removed duplicate frames

--- scripts/removedups.sh
+++ scripts/removedups.sh
@@ -39,6 +39,23 @@ for M in $(awk '{print $2}' $TMPDIR/md5sums.$$ | sort -u); do
   grep $M $TMPDIR/md5sums.$$ | head -1
 done | awk '{print $1}' | sort -n  > $TMPDIR/pckts.$$
 
+wc -l $TMPDIR/md5sums.$$ $TMPDIR/pckts.$$ | awk '
+  NR == 1 { FRAMES = $1 }
+  NR == 2 { LEFT = $1 }
+END {
+  REMOVED = FRAMES - LEFT
+  if ( REMOVED > 0 ) {
+    if ( REMOVED == 1 ) {
+      print "Removed 1 duplicate frame"
+    } else {
+      PERC = ( REMOVED / FRAMES) * 100
+      printf("Removed %d (~%d%%) duplicate frames\n", REMOVED, PERC )
+    }
+  } else {
+    print "No duplicate frames found"
+  }
+}' >&2
+
 #echo "Trick on callflow.short (remove left spaces (left trim))"
 sed 's/^ *//' $DESTDIR/callflow.short > $TMPDIR/callflow.short.$$
 
