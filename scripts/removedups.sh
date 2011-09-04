2cc94eb1b210d319e42c5a4d60b2639da3f1233b
rbos <rbos@8a65f31f-a56f-4bd6-98e8-2755f64920ec>
2011-09-04 19:39:11 +0000

    added missing 'are'

--- scripts/removedups.sh
+++ scripts/removedups.sh
@@ -78,7 +78,7 @@ elif [[ $MODE = "REMOVE_MIRROR_DUPS_2" ]]; then
   # here anyway again, as this "remove duplicated frames mode" seems to be
   # the better one.  The others may be removed, including the md5sum
   # processing done above.
-  # The code block /^[^#]/ takes care that comments skipped
+  # The code block /^[^#]/ takes care that comments are skipped
   awk -F"|" '/^[^#]/ {print $3, $1}' $DESTDIR/callflow.short | while read FRAME TIME; do
     MD5SUM=$(mkmd5sum $DESTDIR/frames/Frame${FRAME}.html | sed 's/ .*//')
     echo "$FRAME|$TIME|$MD5SUM"
